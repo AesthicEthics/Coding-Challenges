1750810755ce84cac2c7b1a85959e14d64f4f302
Aryan Thakur <70349501+AesthicEthics@users.noreply.github.com>
2020-12-20 22:11:57 -0500

    Update RSA.py

--- RSA.py
+++ RSA.py
@@ -3,11 +3,12 @@ Problem:
 
 When a credit card number is sent through the Internet it must be protected so that other people cannot see it. Many web browsers use a protection based on "RSA Numbers."
 
-A number is an RSA number if it has exactly four divisors. In other words, there are exactly four numbers that divide into it evenly. For example, 10 is an RSA number because it
-has exactly four divisors (1,2,5,10). 12 is not an RSA number because it has too many divisors (1,2,3,4,6,12). 11 is not an RSA number either. There is only one RSA number in the
-range 10…12.
+A number is an RSA number if it has exactly four divisors. In other words, there are exactly four numbers that divide into it evenly. For example, 10 is an RSA number
+because it has exactly four divisors (1,2,5,10). 12 is not an RSA number because it has too many divisors (1,2,3,4,6,12). 11 is not an RSA number either. There is only 
+one RSA number in therange 10…12.
 
-Write a program that inputs a range of numbers and then counts how many numbers from that range are RSA numbers. You may assume that the numbers in the range are less than 1000.
+Write a program that inputs a range of numbers and then counts how many numbers from that range are RSA numbers. You may assume that the numbers in the range are less 
+than 1000.
 
 """
 
