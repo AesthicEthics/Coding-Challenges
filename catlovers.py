df1f99c2c18d00dfa3b94dd766b2f7e5aa46af0b
Aryan Thakur <70349501+AesthicEthics@users.noreply.github.com>
2020-12-24 12:12:29 -0500

    Update catlovers.py

--- catlovers.py
+++ catlovers.py
@@ -1,3 +1,16 @@
+"""
+Kirito loves cats. He joined a survey with N people and a single question:
+
+Do you like cats more or dogs more? You may only choose one.
+
+All N people are required to answer. Each person answered with cats or dogs.
+
+If someone responds with cats, then he or she likes cats more. Otherwise, he or she likes dogs more.
+
+Given that all N people answered with either cats or dogs, what was the total preference?
+
+Note: If the number of responses for cats and dogs are equal, then output equal.
+"""
 a = int(input())
 d =[]
 for i in range(0,a):
