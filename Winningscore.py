fe191a48ae07322a4daae5ccf23b8482fc0e4d57
Aryan Thakur <70349501+AesthicEthics@users.noreply.github.com>
2021-01-01 21:09:32 -0500

    Create Winningscore.py

---
+++ Winningscore.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+
+g = ((3*(a))+(2*(b))+(1*(c)))
+h = ((3*(d))+(2*(e))+(1*(f)))
+
+if g > h:
+    print("A")
+elif g < h:
+    print("B")
+else:
+    print("T")
